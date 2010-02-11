4e895b265da6524d0ab7c0eb66158c61cfba74c4
Joe <joe@joe-vm.(none)>
2010-02-11 00:32:23 -0500

    Added some basic stuff for building a tree.

--- proj01/parser.ads
+++ proj01/parser.ads
@@ -2,9 +2,22 @@
 -- COL Gene Ressler, 1 December 2007
 
 package Parser is
-
+   
+   type Node_T is abstract tagged null record;
+   
    -- This is a placeholder for future syntax tree node pointer type.
-   type Node_A is access all Integer;
+   type Node_A is access Node_T;
+   
+   type Model_Node_T is new Node_T with record
+      Description_Section : Node_A;
+      Instance_Section : Node_A;
+   end record;
+   
+   type Description_Section_Node_T is new Node_T with record
+      Named_Description_List : Integer;
+   end record;
+     
+   
 
    -- Parse the input string as a TL command.  Raise a Syntax_Error exception
    -- if parsing fails.  Scanner may also raise Bad_Character if it sees
@@ -16,4 +29,5 @@ package Parser is
                     Debug_Mode : in Boolean := False);
 
    Syntax_Error : exception;
+   
 end Parser;
